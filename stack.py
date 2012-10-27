44768087d87ef01205bcdbd7af3533f017284451
glaslos <glaslos@gmail.com>
2012-10-27 15:20:00 +0200

    Minor code clean-ups

--- stack.py
+++ stack.py
@@ -2,7 +2,9 @@
 # Copyright (C) 2012 Buttinsky Developers.
 # See 'COPYING' for copying permission.
 
+
 class Message(object):
+
     def __init__(self, data="", left=""):
         self.data = data
         if not left:
@@ -10,32 +12,46 @@ class Message(object):
         else:
             self.left = left
 
+
 class Layer(object):
+
     def __init__(self, plugin, lower=None, upper=None):
         self.plugin = plugin
         self.upper = upper
         self.lower = lower
+
     def setLower(self, lower):
         self.lower = lower
+
     def setUpper(self, upper):
         self.upper = upper
+
     def transmit(self, msg):
         msg = self.plugin.transmit(msg)
         if self.lower != None:
             self.lower.transmit(msg)
+
     def receive(self, msg):
         msg = self.plugin.receive(msg)
         if self.upper != None:
             self.upper.receive(msg)
         else:
             self.transmit(msg)
+
     def settings(self, settings):
         self.plugin.settings(settings)
 
+
 class LayerPlugin(object):
+
+    def __init__(self):
+        pass
+
     def transmit(self, msg):
         raise NotImplementedError
+
     def receive(self, msg):
         raise NotImplementedError
+
     def settings(self, settings):
         raise NotImplementedError
