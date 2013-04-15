75a6d82e3e9e09e3ba1655e44055837043141774
pjlantz <patrik@pjlantz.com>
2013-04-15 19:56:10 +0200

    Refactored Netzob protocol model

--- behaviors/custom_irc_protocol.py
+++ behaviors/custom_irc_protocol.py
@@ -1,43 +1,33 @@
 #!/usr/bin/env python
-# Copyright (C) 2012 Buttinsky Developers.
+# Copyright (C) 2013 Buttinsky Developers.
 # See 'COPYING' for copying permission.
 
 from stack import LayerPlugin, Message
+from netzobparse import NetzobModelParser
 
-import binascii
-from xml.dom import minidom
 
 class CustomIRCProtocol(LayerPlugin):
 
-    def __init__(self):
-        self.state = None
-        self.STATES = None
-        self.SYMBOLS = None
-        self.TRANSITIONS = None
-
     def settings(self, settings):
         self.settings = settings["protocol"]
+        self.stateMachine = NetzobModelParser('behaviors/models/customNetzobIRCModel.xml')
 
     def receive(self, msgs):
-        if self.STATES == None:
-            self.parse_model()
-        
         messages = []
         for m in msgs.data:
-            print m
             if "command" in m:
-                if m["command"] == "JOIN" and self.state == None:
-                    reply_msg = self.init()
+                reply_msg = None
+                if m["command"] == "JOIN":
+                    reply_msg = self.stateMachine.handleInput(None, self.settings)
                     if reply_msg == None:
                         break
                     reply = dict()
                     reply["command"] = "PRIVMSG"
                     reply["args"]= self.settings["channel"].encode("ascii") + " :" + reply_msg
                     messages.append(reply)
-                if m["command"] == "PRIVMSG" and self.state != None:
-                    reply_msg = self.handleState(m["args"][1])
+                if m["command"] == "PRIVMSG":
+                    reply_msg = self.stateMachine.handleInput(m["args"][1], self.settings)
                     if reply_msg == None:
-                        #messages.append(m)
                         break
                     reply = dict()
                     reply["command"] = "PRIVMSG"
@@ -45,99 +35,8 @@ class CustomIRCProtocol(LayerPlugin):
                     messages.append(reply)
                 else:
                     messages.append(m)
-    
         return Message(messages, msgs.left)
 
     def transmit(self, msg):
         return msg
 
-    def init(self):
-        initState = self.STATES[0]["id"]
-        trans = self.TRANSITIONS[initState]
-        if trans["input"] == None:
-            self.state = trans["endState"]
-            symbol = trans["outputs"][0]
-            msg = self.SYMBOLS[symbol]
-            return msg
-
-    def handleState(self, input):
-        msg = None
-        trans = self.TRANSITIONS[self.state]
-        inputSymbol = self.SYMBOLS[trans["input"]]
-        
-        if input == inputSymbol:    
-            outputSymbol = self.SYMBOLS[trans["outputs"][0]]
-            if "<settings($nick)>" in outputSymbol:
-                msg = outputSymbol.replace("<settings($nick)>", self.settings["nick"].encode("ascii"))
-        else:
-            outputSymbol = self.SYMBOLS[trans["outputs"][0]]
-            if "<var($domain)>" in outputSymbol:
-                inputCmd = inputSymbol.split("<var($domain)>")[0].strip()
-                if inputCmd in input:
-                    domain = input.split(inputCmd)[1].strip()
-                    msg = outputSymbol.replace("<var($domain)>", domain)
-        if msg != None:
-            self.state = trans["endState"]
-            return msg    
-
-
-    def bin2Ascii(self, bin_text):
-        return ''.join(chr(int(bin_text[i:i+8], 2)) for i in xrange(0, len(bin_text), 8))
-
-    def parse_model(self):
-        self.STATES = []
-        self.SYMBOLS = {}
-        self.TRANSITIONS = {}
-
-        xmldoc = minidom.parse('behaviors/models/customNetzobIRCModel.xml')
-        grammar = xmldoc.getElementsByTagName('netzob:grammar')
-        automata = grammar[0].getElementsByTagName('netzob:automata')
-        states = grammar[0].getElementsByTagName('netzob:states')
-        state = grammar[0].getElementsByTagName('netzob:state')
-        transitions = grammar[0].getElementsByTagName('netzob:transitions')
-        transition = grammar[0].getElementsByTagName('netzob:transition')
-        symbols = xmldoc.getElementsByTagName('netzob:symbols')
-        symbol = symbols[0].getElementsByTagName('netzob:symbol')
-
-        symbolID = 0
-        for s in symbol:
-            symbolID = s.getAttribute("id").encode("ascii")
-            f = s.getElementsByTagName("netzob:field")
-            fields = s.getElementsByTagName("netzob:fields")
-            string = ""
-            for field in fields:
-                f = field.getElementsByTagName("netzob:field")
-                for entry in f:
-                    format = entry.getElementsByTagName("netzob:format")
-                    var = entry.getElementsByTagName("netzob:variable")
-                    type = var[0].getElementsByTagName("netzob:type")
-                    value = var[0].getElementsByTagName("netzob:originalValue")
-                    if len(value) > 0:
-                        string = string + self.bin2Ascii(value[0].firstChild.data)
-            self.SYMBOLS[symbolID] = string
-
-        for node in state:
-            stateID = node.getAttribute("id").encode("ascii")
-            stateName = node.getAttribute("name").encode("ascii")
-            self.STATES.append({"id": stateID, "name": stateName})
-
-        for node in transition:
-            start = node.getElementsByTagName("netzob:startState")
-            startState = start[0].firstChild.data.encode("ascii")
-
-            end = node.getElementsByTagName("netzob:endState")
-            endState =  end[0].firstChild.data.encode("ascii")
-            input = node.getElementsByTagName("netzob:input")
-            stateInput = input[0].getAttribute("symbol").encode("ascii")
-            if stateInput == "EmptySymbol":
-                stateInput = None
-            outputs = node.getElementsByTagName("netzob:outputs")
-            for outs in outputs:
-                out = outs.getElementsByTagName("netzob:output")
-            outSymbol = list()
-            for o in out:
-                outSymbol.append(o.getAttribute("symbol").encode('ascii'))
-            if len(outSymbol) == 0:
-                outSymbol = None
-            self.TRANSITIONS[startState] = {"endState": endState, "input": stateInput, "outputs": outSymbol}
-         
---
+++ netzobparse.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# Copyright (C) 2013 Buttinsky Developers.
+# See 'COPYING' for copying permission.
+
+import binascii
+from xml.dom import minidom
+
+
+class NetzobModelParser(object):
+
+    def __init__(self, path):
+        self.STATES = []
+        self.SYMBOLS = {}
+        self.TRANSITIONS = {}
+        self.state = None
+        self.__parse_model(path)
+
+    def handleInput(self, input, settings):
+        msg = None
+        if self.state == None:
+            initialState = self.STATES[0]["id"]
+            trans = self.TRANSITIONS[initialState]
+            if trans["input"] == None:
+                self.state = trans["endState"]
+                symbol = trans["outputs"][0]
+                msg = self.SYMBOLS[symbol]
+                return msg
+        
+        trans = self.TRANSITIONS[self.state]
+        inputSymbol = self.SYMBOLS[trans["input"]]
+        
+        if input == inputSymbol:    
+            outputSymbol = self.SYMBOLS[trans["outputs"][0]]
+            if "<settings($nick)>" in outputSymbol:
+                msg = outputSymbol.replace("<settings($nick)>", settings["nick"].encode("ascii"))
+        else:
+            outputSymbol = self.SYMBOLS[trans["outputs"][0]]
+            if "<var($domain)>" in outputSymbol:
+                inputCmd = inputSymbol.split("<var($domain)>")[0].strip()
+                if inputCmd in input:
+                    domain = input.split(inputCmd)[1].strip()
+                    msg = outputSymbol.replace("<var($domain)>", domain)
+        if msg != None:
+            self.state = trans["endState"]
+        return msg
+
+    def __bin2Ascii(self, bin_text):
+        return ''.join(chr(int(bin_text[i:i+8], 2)) for i in xrange(0, len(bin_text), 8))
+
+    def __parse_model(self, path):
+        xmldoc = minidom.parse(path)
+        grammar = xmldoc.getElementsByTagName('netzob:grammar')
+        automata = grammar[0].getElementsByTagName('netzob:automata')
+        states = grammar[0].getElementsByTagName('netzob:states')
+        state = grammar[0].getElementsByTagName('netzob:state')
+        transitions = grammar[0].getElementsByTagName('netzob:transitions')
+        transition = grammar[0].getElementsByTagName('netzob:transition')
+        symbols = xmldoc.getElementsByTagName('netzob:symbols')
+        symbol = symbols[0].getElementsByTagName('netzob:symbol')
+
+        symbolID = 0
+        for s in symbol:
+            symbolID = s.getAttribute("id").encode("ascii")
+            f = s.getElementsByTagName("netzob:field")
+            fields = s.getElementsByTagName("netzob:fields")
+            string = ""
+            for field in fields:
+                f = field.getElementsByTagName("netzob:field")
+                for entry in f:
+                    format = entry.getElementsByTagName("netzob:format")
+                    var = entry.getElementsByTagName("netzob:variable")
+                    type = var[0].getElementsByTagName("netzob:type")
+                    type = type[0].firstChild.data
+                    value = var[0].getElementsByTagName("netzob:originalValue")
+                    if len(value) > 0:
+                        if type == "Binary":
+                            string = string + self.__bin2Ascii(value[0].firstChild.data)
+            self.SYMBOLS[symbolID] = string
+
+        for node in state:
+            stateID = node.getAttribute("id").encode("ascii")
+            stateName = node.getAttribute("name").encode("ascii")
+            self.STATES.append({"id": stateID, "name": stateName})
+
+        for node in transition:
+            start = node.getElementsByTagName("netzob:startState")
+            startState = start[0].firstChild.data.encode("ascii")
+
+            end = node.getElementsByTagName("netzob:endState")
+            endState =  end[0].firstChild.data.encode("ascii")
+            input = node.getElementsByTagName("netzob:input")
+            stateInput = input[0].getAttribute("symbol").encode("ascii")
+            if stateInput == "EmptySymbol":
+                stateInput = None
+            outputs = node.getElementsByTagName("netzob:outputs")
+            for outs in outputs:
+                out = outs.getElementsByTagName("netzob:output")
+            outSymbol = list()
+            for o in out:
+                outSymbol.append(o.getAttribute("symbol").encode('ascii'))
+            if len(outSymbol) == 0:
+                outSymbol = None
+            self.TRANSITIONS[startState] = {"endState": endState, "input": stateInput, "outputs": outSymbol}
+
+         
